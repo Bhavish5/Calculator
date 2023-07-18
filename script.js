79af93dfb859d84f9587bd2401351b2714d730cf
Bhavish5 <bhavish7turaga@gmail.com>
2023-07-18 22:07:11 +0530

    Added eval in javascript

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ function inputNum(num){
     var string = document.getElementById("input").value;
     document.getElementById("input").value = string + num;
 }
-function clear(){
-    document.getElementById("input").value = " ";
+function clearField(){
+    document.getElementById("input").value = "";
+}
+function equalsAns(){
+    var ans = document.getElementById("input").value;
+    var printAns = eval(ans);
+    document.getElementById("input").value = printAns;
 }
